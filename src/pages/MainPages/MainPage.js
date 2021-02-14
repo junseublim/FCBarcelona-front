707e2be7d5e252782eb361efb3d249c7d68baa06
2016026480 <junslim11@gmail.com>
2021-02-14 18:03:08 +0900

    add: main news

--- src/pages/MainPages/MainPage.js
+++ src/pages/MainPages/MainPage.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Squad from './Squad';
-
+import main_img from '../../static/img/main_img.webp'
 const MainPage = () => {
     return (
         <Switch>
             <Route exact path="/">
-                <div>main</div>
+                <div className="main-view">
+                    <div className="img-wrapper">
+                        <img src={main_img} alt="" className="main-img"/>
+                        <img src={main_img} alt="" className="blur-img"/>
+                    </div>
+                    <div className="article">
+                        <div className="article-title">
+                        Players to wear names in Chinese in build-up to game with Alavés
+                        </div>
+                    </div>
+                    
+                </div>
             </Route>
             <Route path="/squad">
                 <Squad></Squad>
