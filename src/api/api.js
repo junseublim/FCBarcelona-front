e27213a8f4fbd8a562dff2b9a7abf4e4f41e4329
junseublim <junslim11@gmail.com>
2021-05-27 13:37:04 +0000

    fix: api config ignore

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
-import { dev_uri } from "./config.js";
+import { uri } from "./config.js";
 import axios from 'axios';
 
-export const get_squad = () => axios.get(`${dev_uri}/players`).then((res) => res.data);
-export const get_matches = () => axios.get(`${dev_uri}/matches`).then((res) => res.data);
-export const get_teams = () => axios.get(`${dev_uri}/teams`).then((res) => res.data);
-export const get_news = () => axios.get(`${dev_uri}/news`).then((res) => res.data);
-export const get_transfers = () => axios.get(`${dev_uri}/transfers`).then((res) => res.data);
+export const get_squad = () => axios.get(`${uri}/players`).then((res) => res.data);
+export const get_matches = () => axios.get(`${uri}/matches`).then((res) => res.data);
+export const get_teams = () => axios.get(`${uri}/teams`).then((res) => res.data);
+export const get_news = () => axios.get(`${uri}/news`).then((res) => res.data);
+export const get_transfers = () => axios.get(`${uri}/transfers`).then((res) => res.data);
